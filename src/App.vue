0ebf72ee367fdac309b9b4556098c7a51c6774a1
ThazinWai63 <e1332403@u.nus.edu>
2025-06-12 22:53:05 +0800

    Commit002_LandingPageNavigationWithButtonAction_LoginPage

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,10 @@ import NavBar from './components/nav/NavBar.vue'
 </script>
 
 <template>
-  <main class="min-h-screen bg-gray-100">
-    <!-- <div class="p-4 bg-blue-500 text-white text-center">
-      <h1 class="text-2xl font-bold">Testing Tailwind CSS</h1>
-    </div> -->
+  <div>
     <NavBar />
-  </main>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
